0dfe40ff785385f5c69d0cd19d46841fee84afc9
Mohammed Noori <kmanoorib@gmail.com>
2019-07-07 03:26:28 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+  
+  def time_format
+    "#{self.appointment_datetime.%B} #{self.appointment_datetime.$d}, #{self.appointment_datetime.%Y} at #{self.appointment_datetime.%k}:#{self.appointment_datetime.%M}"
+  end
+  
 end
